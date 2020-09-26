ac5ed3af2a9f850df9fb31e09cf67230014379a3
Daniel Jack <jackd68ed@googlemail.com>
2020-09-26 15:36:45 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dict-to-dataclass",
+    version="0.0.1",
+    author="Daniel Jack",
+    author_email="jackd68ed@googlemail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
