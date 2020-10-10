2147a9c9d76a1708f6892aa9738feb941fc7cfb9
Daniel Jack <jackd68ed@googlemail.com>
2020-10-10 15:39:36 +0100

    Added a from_dict constructor to the base class

--- dict_to_dataclass/base_class.py
+++ dict_to_dataclass/base_class.py
@@ -1,6 +1,9 @@
+from dict_to_dataclass import dataclass_from_dict
+
+
 class DataclassFromDict:
     """A base class for dataclasses that can be created using `dataclass_from_dict`"""
 
-    # True if the origin dictionary's keys are in camelCase. Used when `field_from_dict`s are created
-    # and `dict_key` is omitted.
-    dict_has_camel_case_keys = False
+    @classmethod
+    def from_dict(cls, origin_dict: dict):
+        return dataclass_from_dict(cls, origin_dict)
---
+++ dict_to_dataclass/tests/test_base_class.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from unittest import TestCase
+from dict_to_dataclass.base_class import DataclassFromDict
+from dict_to_dataclass import field_from_dict
+
+
+class BaseClassTestCase(TestCase):
+    def test_should_construct_instance_from_dict(self):
+        @dataclass
+        class MyDataclass(DataclassFromDict):
+            my_field: str = field_from_dict()
+
+        expected = MyDataclass(my_field="field value")
+
+        self.assertEqual(expected, MyDataclass.from_dict({"my_field": "field value"}))
