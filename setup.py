b80f0c2657306d776c5413958c099c2b2859ad70
Daniel Jack <jackd68ed@googlemail.com>
2020-10-11 13:50:21 +0100

    Version bump 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dict_to_dataclass",
-    version="0.0.1",
+    version="0.0.2",
     author="Daniel Jack",
     author_email="jackd68ed@googlemail.com",
     long_description=long_description,
