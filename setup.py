b5902e5fa50b715a446575de3e2be4de0a1844ef
Daniel Jack <jackd68ed@googlemail.com>
2020-10-30 15:17:44 +0000

    Get the package requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="dict_to_dataclass",
     version="0.0.4",
@@ -17,5 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
-    install_requires=["python-dateutil==2.8.1"],
+    install_requires=requirements,
 )
